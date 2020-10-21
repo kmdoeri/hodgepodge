42708c70a461fb6fbdf2481b93ea615c06ffcbc0
Kristin Doering <kmdoeri1996@gmail.com>
2020-10-21 10:45:17 -0500

    initial commit

---
+++ src/main/java/HodgepodgeController.java
@@ -0,0 +1,25 @@
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class HodgepodgeController {
+
+    @GetMapping("/name")
+    public String myName() {
+        return "Kristin Doering";
+    }
+
+    @PostMapping("/post")
+    public void doPost() {
+
+    }
+
+    @PutMapping("/put")
+    public void doPut() {
+
+    }
+
+    @DeleteMapping("/delete")
+    public void doDelete() {
+
+    }
+}
